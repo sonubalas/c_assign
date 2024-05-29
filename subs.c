a78548285b82831d6b6deda544bf35786ae5766f
kumawatsonu <kumawatsonu812002@gmail.com>
2024-05-29 15:30:48 +0530

    first assignment

---
+++ subs.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=34,b=5,c;
+    
+    c=a-b;
+    printf("substriction is = %d\n",c);
+    
+    return 0;
+}
---
+++ userinout.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+int a;
+printf("enter the value of a ");
+scanf("%d",&a);
+int b;
+printf("enter the value of b ");
+scanf("%d",&b);
+
+int c;
+
+c=a+b;
+
+printf(" the addition is = %d",c);
+}
